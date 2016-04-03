5d379d7a558849aed7b9485969735675cce8e65f
sams <sams@electr0n1cl4b>
2016-04-03 15:18:37 -0430

    se agrego modulo de generar orden

--- farmacia_MammaMia/farmacia.c
+++ farmacia_MammaMia/farmacia.c
@@ -8,6 +8,7 @@
  \***********************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXNOMBRE 20	//Define el numero maximo de caracteres para el nombre de un producto
 #define MAXNUMPRODUCT 5
@@ -17,41 +18,46 @@ typedef struct{
 	//int i_codProduct;				// codigo del producto
 	char s_Nomb[MAXNOMBRE];	// nombre del producto
 	float f_Cost;			// precio del producto
-	int i_Vent;	//cantidad de items vendidos del producto
+	int i_VentDia;	//cantidad de items vendidos del producto en el día
+	int i_Venttot; //cantidad total de items vendidos del producto
 	int i_Stock;// contidad restante de items del producto
 	
 } TipoProducto;
 
 
 TipoProducto arr_producto[MAXNUMPRODUCT]; // El indice del vector corresponde al codigo del producto comenzando este en 1
+int i_maxProductLeido; // indica el numero maximo ee productos leidos
 
 int leerArchivo ();
 int leerArchivoVentas();
 void mostrarInventario();
 void ordenCompra();
-		
+void llenarArchivo();
+void imprimirOrden();
+void esperaEnter();
 
 int main()
 {
-	int i_codProduct;
-	int i_maxProductLeido;
+	//int i_codProduct;
 	int i_listaMenu;
 	int i_salida = 0;
 	enum listMenu { llenar_archivo = 1, cargar_archivo , mostrar_inventario, mostrar_ventas_dia, generar_orden, salir};
 //	TipoProducto arr_producto[MAXNUMPRODUCT]; // El indice del vector corresponde al codigo del producto comenzando este en 1
 //	leerArchivo();
-	do{
-		printf("bienvenido..........\n");
+	system("clear");
+	printf("bienvenido..........\n");
+	do{			
 		printf("1) llenar archivo de inventario manualmente.\n");
 		printf("2) cargar archivo de inventario\n");
 		printf("3) mostrar inventario\n");
 		printf("4) mostrar ventas del dia\n");
 		printf("5) generar orden de compra\n");
 		printf("6) salir\n");
-		printf("seleccione una op	cion: ");
+		printf("seleccione una opcion: ");
 		scanf("%d",&i_listaMenu);
 		switch (i_listaMenu){
 			case llenar_archivo: //llenar archivo de inventario manualmente
+				llenarArchivo();
 				break;
 			case cargar_archivo: //cargar archivo de inventario
 				leerArchivo();
@@ -63,28 +69,37 @@ int main()
 				leerArchivoVentas();
 				break;
 			case generar_orden: //generar la orden de compras
+				ordenCompra();
 				break;
 			case salir:
 				i_salida = 1;
 				break;
+			default:
+				printf("Ha introducido una opción inválida\n");
+				printf("Intentelo nuevamente\n\n\n");
+				break;
 		}
+		system("clear");
 	}while(!i_salida);
 	
-	printf("\n\ngracias por utilizar nuestro sistema...\n\n\n");
-	/*for( i_codProduct = 1; i_codProduct <= i_maxProductLeido; i_codProduct++){
-		printf(" producto: codigo %d descripcion %15s precio es %3.2f\n",i_codProduct,arr_producto[i_codProduct].s_Nomb,
-																					arr_producto[i_codProduct].f_Cost);
-	}*/	
+	printf("\n\nGracias por utilizar nuestro sistema...\n\n\n");
+
 	
 	return 0;
 }
 
+// Funcion para llenar el archivo de inventario manualmente
+void llenarArchivo(){
+	system("clear");
+	printf("Esta funcion no está disponible en la versión actual del sistema...\n\n\n");
+	esperaEnter();// funcion para esperar que el usuario presione enter
+}
 
 // Funcion para leer el archivo de datos con informacion de los productos
 int leerArchivo () //retorna la cantidad de productos leidos
 {	
 	int i_codProduct; //indica el codigo del producto
-	int i_maxProductLeido; // indica el numero maximo ee productos leidos
+
 	
 	// Se crea el buffer para la lectura del archivo que contiene la informacion de productos ( codigo,nombre,preciounit )
 	FILE *archivoInfo;
@@ -97,10 +112,11 @@ int leerArchivo () //retorna la cantidad de productos leidos
 		printf("error en la lectura del archivo de informacion de productos");
 	}else{
 		//lectura del archivo y llenado del vector
-		i_codProduct = 1;
+		i_codProduct = 1;	
+		system("clear");
 		do{
-			printf("leyendo archivo de informacion, producto %d \n",i_codProduct);
-			fscanf(archivoInfo," %s %f",arr_producto[i_codProduct].s_Nomb,&arr_producto[i_codProduct].f_Cost);
+			printf(" cargado producto codigo %d ... \n",i_codProduct);
+			fscanf(archivoInfo," %s %f %d",arr_producto[i_codProduct].s_Nomb,&arr_producto[i_codProduct].f_Cost,&arr_producto[i_codProduct].i_Stock);
 			i_codProduct++;
 			
 			//se verifica que la lectura de productos del archivo no excedan el espacio del vector donde se van a almacenar los valores
@@ -110,10 +126,11 @@ int leerArchivo () //retorna la cantidad de productos leidos
 			
 		}while((!feof(archivoInfo)) || (i_codProduct == MAXNUMPRODUCT-1));
 		
-		fclose(archivoInfo);
-				
+		fclose(archivoInfo);// cierra el archivo de texto
+		esperaEnter();// funcion para esperar que el usuario presione enter
+
 	}
-	return i_maxProductLeido;
+	return 0;
 }
 	
 
@@ -134,16 +151,18 @@ int leerArchivoVentas(){
 		//i_codProduct = 1;
 		do{
 			fscanf(archivoVentas,"%d,%d",&i_codProduct,&i_ventaAux);
-			arr_producto[i_codProduct].i_Vent = arr_producto[i_codProduct].i_Vent + i_ventaAux;
-			printf("se registro venta del articulo codigo %d por una cantidad de %d \n",i_codProduct,arr_producto[i_codProduct].i_Vent);
+			arr_producto[i_codProduct].i_VentDia = arr_producto[i_codProduct].i_VentDia + i_ventaAux;
+			printf("se registro venta del articulo codigo %d por una cantidad de %d \n",i_codProduct,arr_producto[i_codProduct].i_VentDia);
 		}while(!feof(archivoVentas));
 		fclose(archivoVentas);
 	}
 	for( i_codProduct = 1; i_codProduct <= MAXNUMPRODUCT; i_codProduct++){
 	printf(" producto: codigo %d  descripcion %10s cantidad de ventas %d\n",i_codProduct,arr_producto[i_codProduct].s_Nomb,
-																					arr_producto[i_codProduct].i_Vent);
+																			arr_producto[i_codProduct].i_VentDia);
 		
 	}
+	
+	esperaEnter();
 	
 	return 0;
 }	
@@ -152,9 +171,16 @@ int leerArchivoVentas(){
 // Funcion que muestra el inventario por pantalla para que el usuario pueda visualizar lo que imprimira al archivo
 void mostrarInventario()
 {
+	int i_codProduct;
+	for( i_codProduct = 1; i_codProduct <= i_maxProductLeido; i_codProduct++){
+		printf(" producto: codigo %d descripcion %15s precio es %3.2f  ventas totales %d stock %d\n",i_codProduct,
+																									arr_producto[i_codProduct].s_Nomb,
+																									arr_producto[i_codProduct].f_Cost,
+																									arr_producto[i_codProduct].i_Venttot,
+																									arr_producto[i_codProduct].i_Stock);
+	}
 	
-	
-	
+	esperaEnter();// funcion para esperar que el usuario presione enter
 	
 }
 
@@ -162,9 +188,56 @@ void mostrarInventario()
 // Procedimiento que genera el archivo de orden de compras
 void ordenCompra()
 {
-	int generarOrden;
+	int generarCompra;
 	enum generarOrden { generOrden = 1, noGenerOrden};
-	printf("desea generar orden de compra?");
+	
+	system("clear");
+	imprimirOrden();
+	printf("\n\ndesea generar archivo de orden de compra?\n\n");
+	printf("1) Si\n.");
+	printf("2) No.\n\n");
 	scanf("%d",&generarCompra);
-	switch (generarOrden)
+	switch (generarCompra){
+		case generOrden:
+			system("clear");
+			printf("GENERANDO ARCHIVO....\n\n\n");
+			esperaEnter();
+			break;
+		case noGenerOrden:
+			break;
+	}
+}
+
+void imprimirOrden()
+{
+	int i_codProduct;
+	float f_presupuestado = 0;
+	int i_cantProductos = 0;
+	
+	printf("Farmacia MAMMA MÍA C.A.\n");
+	printf("Esquina de Salas Edif. Turimiquire, PB.");
+	printf("\t\t\t\t Listado de productos a comprar.\n");
+	printf("Codigo del Producto \t Nombre del Producto \t Precio Unitario \t Cantidad a pedir \t Bs presupuestado\n");
+	for( i_codProduct = 1; i_codProduct < i_maxProductLeido; i_codProduct++){
+		if ( arr_producto[i_codProduct].i_VentDia != 0){
+			printf("\t %d \t\t\t %s \t %3.2f \t\t %d \t\t %5.2f \n",i_codProduct,
+															arr_producto[i_codProduct].s_Nomb,
+															arr_producto[i_codProduct].f_Cost,
+															arr_producto[i_codProduct].i_VentDia,
+															arr_producto[i_codProduct].i_VentDia * arr_producto[i_codProduct].f_Cost);
+			i_cantProductos = i_cantProductos + 1;
+			f_presupuestado = f_presupuestado + (arr_producto[i_codProduct].i_VentDia * arr_producto[i_codProduct].f_Cost);
+		}
+	}
+	printf("\n\n Totales: \n\t cantidad de productos %d, \t presupuesto:  %5.2f \n\n\n", i_cantProductos, f_presupuestado);
+	
+	esperaEnter();
+	
+}
+
+void esperaEnter()
+{
+	printf("Presione enter para continuar");
+	getchar(); while(getchar() != '\n'); //linea para detener el sistema y esperar que el usuario teclee enter
+	system("clear");
 }
